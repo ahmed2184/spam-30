e677f9117c20b8ef08037abc7f4eb232c37d06a6
ahmed2184 <44103510+ahmed2184@users.noreply.github.com>
2019-01-31 20:01:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("519853742355644418")
+let channel =     client.channels.get("540589086445010968")
 setInterval(function() {
 channel.send(`asdfasdfasdfasdgausodghaskldfgvbaskhydfalsdfglidf`);
 }, 25)
